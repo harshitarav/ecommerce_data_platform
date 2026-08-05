87edda0345d43323820bc885fe40b8a7c1d8f97e
harshitarav <harshitaravi96@gmail.com>
2026-08-05 20:03:05 +0530

    new changes

--- scripts/build_layer.sh
+++ scripts/build_layer.sh
@@ -10,12 +10,18 @@ mkdir -p layer/python
 
 pip3 install \
     -r layers/requirements.txt \
-    -t layer/python
+    -t layer/python \
+    --no-cache-dir
+
+# Remove unnecessary files to reduce size
+find layer/python -type d -name "__pycache__" -exec rm -rf {} +
+find layer/python -type d -name "tests" -exec rm -rf {} +
+find layer/python -type d -name "test" -exec rm -rf {} +
+find layer/python -type d -name "*.dist-info" -exec rm -rf {} +
+find layer/python -type d -name "*.egg-info" -exec rm -rf {} +
 
 cd layer
 
 zip -r layer.zip python
-
-cd ..
 
 echo "Layer built successfully."
